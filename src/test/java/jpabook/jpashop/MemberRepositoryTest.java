67e4a5593ae5d531f146490dcf7c8d1494689613
KingGodSOO <48608616+KingGodSOO@users.noreply.github.com>
2019-11-25 22:35:33 +0900

    [update]

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,7 +19,10 @@ public class MemberRepositoryTest {
     @Autowired MemberRepository memberRepository;
 
     @Test   // Repository의 save, find 테스트
-    @Transactional  // EntityManager를 통한 모든 데이터 변경은 항상 Transaction 안에서 이루어져야 하므로, 에너테이션 꼭! // Repository 클래스에 걸면 된다! 여기선 그냥 걸어준거.       스프링 프레임워크꺼로 하면 됨.
+    @Transactional  // EntityManager를 통한 모든 데이터 변경은 항상 Transaction 안에서 이루어져야 하므로, 에너테이션 꼭!
+    // Repository 클래스에 걸면 된다! 여기선 테스트용으로 걸어준거. test클래스에 Transactional을 걸면 자동으로 RollBack한다!!
+    // 스프링 프레임워크꺼로 하면 됨.
+    @Rollback(false) // 해당 옵션을 통해 롤백을 억제할 수 있다.
     public void testMember() throws Exception {
         //given
         Member member = new Member();
@@ -31,5 +35,8 @@ public class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member : "+(findMember == member));
+        // 위의 두 코드는 모두 true이다. 같은 영속성 컨텍스트에서 비교했기 때문이다.
     }
 }
